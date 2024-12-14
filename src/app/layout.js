3c5d5ad4b1d07635b89f9712bd9ec7fd11b0fd45
d-vooolk <volk.dmitrii@otr.ru>
2024-12-14 13:53:02 +0300

    add head

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,19 +2,26 @@ import "./globals.css";
 import {META} from "@/constants/metadata";
 import Header from "@/components/Header/Header";
 import Footer from "@/components/Footer/Footer";
+import Head from "next/head.js";
 
 
 export const metadata = {...META.general, robots: String(META.general.robots)}
 
-export default function RootLayout({ children }) {
+export default function RootLayout({children}) {
 
-  return (
-    <html lang="ru">
-      <body>
-      <Header />
-        {children}
-      <Footer />
-      </body>
-    </html>
-  );
+    return (
+        <html lang="ru">
+        <Head>
+            <link rel="icon" href="/favicon.ico"/>
+            <meta charSet="UTF-8"/>
+            <meta name="viewport" content="width=device-width, initial-scale=1"/>
+            <title>Мой сайт</title>
+        </Head>
+        <body>
+            <Header/>
+            {children}
+            <Footer/>
+        </body>
+        </html>
+    );
 }
