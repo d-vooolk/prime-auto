b865e0b4bc6909ac2a195ec1054ab872c1bc208b
d-vooolk <volk.dmitrii@otr.ru>
2024-12-15 22:55:58 +0300

    fix next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true, // Включает строгий режим React
-    output: 'standalone', // Для удобного развертывания в продакшене
 };
 
 export default nextConfig;
