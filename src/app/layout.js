f97ddf1b8d115e598947dd48f91c5b366b27bde7
d-vooolk <volk.dmitrii@otr.ru>
2024-12-18 00:25:38 +0300

    add GTM

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,6 +3,8 @@ import {META} from "@/constants/metadata";
 import Footer from "@/components/Footer/Footer";
 import Head from "next/head.js";
 import {GetAdaptiveHeader} from "@/utils/utils.js";
+import React from "react";
+import { GoogleTagManager } from '@next/third-parties/google'
 
 
 export const metadata = {...META.general, robots: String(META.general.robots)}
@@ -11,6 +13,7 @@ export default function RootLayout({children}) {
 
     return (
         <html lang="ru">
+        <GoogleTagManager gtmId="GTM-PKL79DZC" />
         <Head>
             <link rel="icon" href="/favicon.ico"/>
             <meta charSet="UTF-8"/>
