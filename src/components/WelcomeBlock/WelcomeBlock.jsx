139cf738b245f3b1f24d49a8ac3a94d89b720417
d-vooolk <volk.dmitrii@otr.ru>
2024-12-20 13:03:07 +0300

    disable lazy loading of first image

--- src/components/WelcomeBlock/WelcomeBlock.jsx
+++ src/components/WelcomeBlock/WelcomeBlock.jsx
@@ -22,6 +22,7 @@ const WelcomeBlock = () => {
                     width={1100}
                     height={632.3}
                     className="welcome-car-image"
+                    loading="eager"
                 />
             </div>
             <div className="welcome-block">
