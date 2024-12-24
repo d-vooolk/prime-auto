62bb5ffe1f6f1c92042032392c7c4b525132ddcd
d-vooolk <volk.dmitrii@otr.ru>
2024-12-24 22:17:48 +0300

    change instagram url

--- src/constants/contactsData.js
+++ src/constants/contactsData.js
@@ -5,7 +5,7 @@ export const CONTACTS_DATA = {
 }
 
 export const LINKS = {
-    instagram: 'https://www.instagram.com/primeauto.minsk/',
+    instagram: 'https://www.instagram.com/avtosvet.by/',
     facebook: 'https://www.facebook.com/profile.php?id=61558468265260',
     tiktok: 'https://www.tiktok.com/@prime_auto_minsk',
     youTube: 'https://www.youtube.com/@prime-auto-minsk',
