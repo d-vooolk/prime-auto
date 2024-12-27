ab0a8ed137a213392234834576ea4d682c3eec71
d-vooolk <volk.dmitrii@otr.ru>
2024-12-28 01:41:54 +0300

    add urls to future pages

--- src/constants/navigation.js
+++ src/constants/navigation.js
@@ -8,8 +8,22 @@ export const NAVIGATION_URL_ANCHORS = {
 }
 
 export const NAVIGATION_URL = {
+    // current (worked)
     home: '/',
     newYearSale: '/new-year-sale',
+    polirovkaOkleyka: '/polirovka-i-okleyka-far',
+    remont: '/remont-far',
+    tehObsluzhivaniye: '/tehnicheskoye-obsluzhivaniye-far',
+    uluchsheniyeKachestvaSveta: '/uluchshenie-kachestva-sveta',
+    uslugi: '/uslugi',
+
+    // add
+    price: '/price',
+    works: '/works',
+    contacts: '/contacts',
+    biLed: '/ustanovka-bi-led-moduley-v-faru',
+    polirovka: '/polirovka-far',
+    okleyka: '/okleyka-far',
 };
 
 export const NAVIGATION = [
