5a74da40417304b5f1c322781c44a295a643d932
d-vooolk <volk.dmitrii@otr.ru>
2024-12-28 01:34:50 +0300

    add pages

---
+++ src/app/polirovka-i-okleyka-far/page.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import {META} from "@/constants/metadata";
+import ServicePageTitleContainer
+    from "@/components/_HelperComponents/ServicePageTitleContainer/ServicePageTitleContainer";
+import './styles.css';
+import OurProposalBlock from "@/components/_HelperComponents/OurProposalBlock/OurProposalBlock";
+import Portfolio from "@/components/Portfolio/Portfolio";
+import FormBlock from "@/components/FormBlock/FormBlock";
+import PriceBlock from "@/components/_HelperComponents/PriceBlock/PriceBlock";
+import {lightQualityProposalList, PAGE_TITLE_TEXT, priceDataSource} from "@/app/polirovka-i-okleyka-far/constants";
+
+export const metadata = {...META.lightQuality, robots: String(META.lightQuality.robots)}
+
+const WindowWorks = () => {
+    return (
+        <div className="light-quality-page-wrapper">
+            <ServicePageTitleContainer
+                headText={PAGE_TITLE_TEXT.title}
+                description={PAGE_TITLE_TEXT.description}
+            />
+            <OurProposalBlock list={lightQualityProposalList} />
+            <PriceBlock
+                title={PAGE_TITLE_TEXT.priceTitle}
+                priceDataSource={priceDataSource}
+            />
+            <Portfolio/>
+            <FormBlock/>
+        </div>
+    )
+}
+
+export default WindowWorks;
