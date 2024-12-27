5b7ae353e02f4e9095e10ec5d9c65a0d94c069e1
d-vooolk <volk.dmitrii@otr.ru>
2024-12-28 01:57:43 +0300

    delete minify in config

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true, // Включает строгий режим React
-    swcMinify: true,
     webpack: (config) => {
         config.resolve.extensions.push('.ts', '.tsx'); // Убедитесь, что эти расширения добавлены
         return config;
