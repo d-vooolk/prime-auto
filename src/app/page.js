6d705fa1bc8bffcf5633ef2f661ad6d69164e730
d-vooolk <volk.dmitrii@otr.ru>
2025-01-22 02:00:44 +0300

    delete new year sale

--- src/app/page.js
+++ src/app/page.js
@@ -5,9 +5,6 @@ import ReviewsBlock from "../components/ReviewsBlock/ReviewsBlock.jsx";
 import Portfolio from "../components/Portfolio/Portfolio.jsx";
 import FormBlock from "../components/FormBlock/FormBlock.jsx";
 import ContactsBlock from "../components/ContactsBlock/ContactsBlock.jsx";
-import HideNotification from "../components/_HelperComponents/HideNotification/HideNotification";
-import {HIDE_NOTIFICATION_TEXT} from "../constants/textConstants.js";
-import {NAVIGATION_URL} from "../constants/navigation.js";
 import {META} from "../constants/metadata.ts";
 
 export const metadata = {...META.general, robots: String(META.general.robots)}
@@ -22,10 +19,6 @@ export default function Home() {
           <Portfolio/>
           <FormBlock/>
           <ContactsBlock/>
-          <HideNotification
-              text={HIDE_NOTIFICATION_TEXT}
-              link={NAVIGATION_URL.newYearSale}
-          />
       </main>
   );
 }
