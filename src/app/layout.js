28054ac440424c119fb596f36af1f94c549320f0
volk <nestwedman@gmail.com>
2025-08-21 20:55:56 +0300

    add yandex metric

--- src/app/layout.js
+++ src/app/layout.js
@@ -12,6 +12,20 @@ export default function RootLayout({children}) {
         <GoogleTagManager gtmId="GTM-PKL79DZC" />
         <head>
             <meta name="yandex-verification" content="46166f0eb1874634" />
+
+            <!-- Yandex.Metrika counter -->
+            <script type="text/javascript">
+                (function(m,e,t,r,i,k,a){
+                m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+                m[i].l=1*new Date();
+                for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+                k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
+            })(window, document,'script','https://mc.yandex.ru/metrika/tag.js?id=103843698', 'ym');
+
+                ym(103843698, 'init', {ssr:true, webvisor:true, clickmap:true, ecommerce:"dataLayer", accurateTrackBounce:true, trackLinks:true});
+            </script>
+            <noscript><div><img src="https://mc.yandex.ru/watch/103843698" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+            <!-- /Yandex.Metrika counter -->
         </head>
         <Head>
             <link rel="icon" href="/favicon.ico" />
