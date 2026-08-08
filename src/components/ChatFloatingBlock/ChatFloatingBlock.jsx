4acdb8fcac29829387e154f4113e11f9f407e6ea
Dmitriy <nestwedman@gmail.com>
2026-08-08 13:59:34 +0300

    change bar image

--- src/components/ChatFloatingBlock/ChatFloatingBlock.jsx
+++ src/components/ChatFloatingBlock/ChatFloatingBlock.jsx
@@ -6,15 +6,16 @@ import Image from "next/image";
 import {Tooltip} from "antd";
 import {ChatConfig} from "./config.js";
 import Link from "next/link";
+import {CloseCircleFilled} from "@ant-design/icons";
 
 const ChatFloatingBlock = () => {
-    const [isBarVisible, setIsBarVisible] = useState([true]);
+    const [isBarVisible, setIsBarVisible] = useState(true);
 
     return (
         <div className="chat-block-wrapper">
             {
                 ChatConfig.map((item) => (
-                    <div key={item.id} className={ isBarVisible ? "flex" : "none" }>
+                    <div key={item.id} className={isBarVisible ? "flex" : "none"}>
 
                         <Link href={item.link} target={item.target} rel="noopener noreferrer">
                             <Tooltip title={item.tooltip} placement="left">
@@ -37,15 +38,18 @@ const ChatFloatingBlock = () => {
                 <Tooltip
                     title={isBarVisible ? "Закрыть" : "Связаться"}
                     placement="left"
-                    trigger="focus"
                 >
-                    <Image
-                        src='/dark-logo.svg'
-                        alt="logo"
-                        width={54}
-                        height={54}
-                        className="chat-base-img"
-                    />
+                    {
+                        isBarVisible
+                            ? (<CloseCircleFilled style={{fontSize: "54px", color: "#a2db00"}}/>)
+                            : (<Image
+                                src='/chatIcons/phone.svg'
+                                alt="logo"
+                                width={54}
+                                height={54}
+                                className="chat-base-img"
+                            />)
+                    }
                 </Tooltip>
             </div>
         </div>
